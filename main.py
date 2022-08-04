710eb2a61757cb4ab4ece1e766ccebcafa3415ff
Jhean Bryan <jheanbeyan123@gmail.com>
2022-08-04 19:59:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
-#https://pypi.org/project/youtube.py/   
-#pip install youtube.py
-from youtube import video
-
-#$ sudo apt update
-#$ sudo apt install ffmpeg
-
-
-
-#buscando o link e depois o video
-url = 'https://www.youtube.com/watch?v=uzBxOUtvqyA'
-video = Video(url)
-
-# #Print Title
-# print(v.title)
-
-# #Print Author name
-# print(v.author)
-
-# #Print Channel ID
-# print(v.channel)
-
-# #Print video description
-# print(v.description)
-
-# #Print video length in second
-# print(v.length)
-
-# #Print video keywords in dict
-# print(v.keywords)
-
-# #Print high resolution thumbnail
-# print(v.thumbnail)
-
-# #Print video id
-# print(v.videoId)
-
-# #Print total video views
-# print(v.views)
+#pip install pytube
+from pytube import YouTube
+video = YouTube('https://www.youtube.com/watch?v=_K9YV4t9dzY')
+stream = video.streams.get_by_itag(22)
+stream.download()
